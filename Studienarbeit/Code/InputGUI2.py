d56547a54f319b9afabde6da8bdeaae0b5b79479
Kevin Wrona <wrona.kevin@gmail.com>
2020-03-10 02:04:34 +0100

    File too big
    Former-commit-id: 7cfe6a098ade3b4cd4ae1ec532ce5a9724e84dfc

--- Studienarbeit/Code/InputGUI2.py
+++ Studienarbeit/Code/InputGUI2.py
@@ -1,24 +1,24 @@
-import cv2
+import cv2 as cv
 
-face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-cam = cv2.VideoCapture(0)
-cv2.namedWindow("InputGUI")
+face_cascade = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+cam = cv.VideoCapture(0)
+cv.namedWindow("InputGUI")
 img_counter = 0
 while True:
     ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
-        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    cv2.imshow("InputGUI", img)
+        img = cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv.imshow("InputGUI", img)
     if not ret:
         break
-    k = cv2.waitKey(1)
+    k = cv.waitKey(1)
     if k%256 == 27: # ESC pressed, closing the window
         break
     elif k%256 == 32: # SPACE pressed, save image as input 
-        cv2.imwrite("InputGUI{}.png".format(img_counter), img)
+        cv.imwrite("InputGUI{}.png".format(img_counter), img)
         img_counter += 1
 
 cam.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
